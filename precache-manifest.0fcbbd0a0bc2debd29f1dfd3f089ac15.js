876394b8427374f708dc2bb2285f15ef1f1dff2b
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-10-12 08:40:45 +0200

    Updates

---
+++ precache-manifest.0fcbbd0a0bc2debd29f1dfd3f089ac15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff6355fbf6acf81e6a45bc365250e11",
+    "url": "/sml-test/index.html"
+  },
+  {
+    "revision": "56e658bb558c7753c6b5",
+    "url": "/sml-test/static/css/main.c502b1c5.chunk.css"
+  },
+  {
+    "revision": "f713f4ab4c0f58ea1806",
+    "url": "/sml-test/static/js/2.6b227121.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sml-test/static/js/2.6b227121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e658bb558c7753c6b5",
+    "url": "/sml-test/static/js/main.1bc7a338.chunk.js"
+  },
+  {
+    "revision": "98272f9e06597780c04e",
+    "url": "/sml-test/static/js/runtime-main.b2f82d68.js"
+  },
+  {
+    "revision": "48fa4dc96042b459ec496857904b083b",
+    "url": "/sml-test/static/media/AvertaDemoPE-Regular.48fa4dc9.woff"
+  },
+  {
+    "revision": "71e7b170aa5d530caecbe5f749efb0d4",
+    "url": "/sml-test/static/media/AvertaDemoPE-Regular.71e7b170.eot"
+  },
+  {
+    "revision": "a24a4f274ea7f823eb9688922919965d",
+    "url": "/sml-test/static/media/AvertaDemoPE-Regular.a24a4f27.woff2"
+  }
+]);
